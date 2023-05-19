250c0e71ca57e3d60a9fc6da9a652af522860e5a
jha2ee <50551231+jha2ee@users.noreply.github.com>
2023-05-19 17:14:01 +0900

    Initial layout in navigation
    초기 레이아웃 설정
    main activity에서 버튼 연결 (navigation activity)

--- IndoorLocalization/app/src/main/java/com/example/indoorlocalization/NavigationActivity.java
+++ IndoorLocalization/app/src/main/java/com/example/indoorlocalization/NavigationActivity.java
@@ -1,6 +1,7 @@
 package com.example.indoorlocalization;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +12,7 @@ public class NavigationActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+        Toast.makeText(getApplicationContext(), "경로 안내를 시작합니다.\n인터넷 연결이 끊어지지 않게 주의하세요.", Toast.LENGTH_LONG).show();
     }
 }
