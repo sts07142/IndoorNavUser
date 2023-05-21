db7df1124f9081f041c5dcb182885a22916334e5
jha2ee <50551231+jha2ee@users.noreply.github.com>
2023-05-21 20:47:45 +0900

    java 변수 설정
    NavigationActivity.java에서 변수 설정,
    xml파일에서 findView,
    방향 이미지 회전 함수 생성

--- IndoorLocalization/app/src/main/java/com/example/indoorlocalization/NavigationActivity.java
+++ IndoorLocalization/app/src/main/java/com/example/indoorlocalization/NavigationActivity.java
@@ -1,6 +1,8 @@
 package com.example.indoorlocalization;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -8,11 +10,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class NavigationActivity extends AppCompatActivity {
 
+    TextView remained_distance; //남은 거리 표시
+    TextView address_point; //출발지, 목적지
+    ImageView direction; //안내 방향 표시
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
+        /* find View */
+        remained_distance = findViewById(R.id.remained_distance);
+        address_point = findViewById(R.id.address);
+        direction = findViewById(R.id.direction);
+        /* variables */
+        int dist = 0;
+        String start_point = "출발지 테스트", dest_point = "목적지 테스트";
+
+        remained_distance.setText("남은 거리 : " + String.valueOf(dist));
+        String sentence = "출발지 : " + start_point + "\n목적지 : " + dest_point;
+        address_point.setText(sentence);
+
+        /* Rotate image view(user direction) */
+        // sample
+        onDirectionRotate(30);
         Toast.makeText(getApplicationContext(), "경로 안내를 시작합니다.\n인터넷 연결이 끊어지지 않게 주의하세요.", Toast.LENGTH_LONG).show();
+    }
+
+    // function : 경로에 따라 유저이미지의 방향 회전시키기
+    protected void onDirectionRotate(int value) {
+        // left, right, front, back
+        direction.setRotation(41);
+
     }
 }
