f01ecfa66a0e0b6107ad8fff4029663bc9ee4a0f
jha2ee <50551231+jha2ee@users.noreply.github.com>
2023-05-21 20:51:28 +0900

    Update NavigationActivity.java
    경고문구 해결
    value값 조절

--- IndoorLocalization/app/src/main/java/com/example/indoorlocalization/NavigationActivity.java
+++ IndoorLocalization/app/src/main/java/com/example/indoorlocalization/NavigationActivity.java
@@ -27,20 +27,21 @@ public class NavigationActivity extends AppCompatActivity {
         int dist = 0;
         String start_point = "출발지 테스트", dest_point = "목적지 테스트";
 
-        remained_distance.setText("남은 거리 : " + String.valueOf(dist));
-        String sentence = "출발지 : " + start_point + "\n목적지 : " + dest_point;
-        address_point.setText(sentence);
+        String sentence_dist = "남은 거리 : " + dist;
+        remained_distance.setText(sentence_dist);
+        String sentence_route = "출발지 : " + start_point + "\n목적지 : " + dest_point;
+        address_point.setText(sentence_route);
 
         /* Rotate image view(user direction) */
-        // sample
-        onDirectionRotate(30);
+        int rotate = 0; // sample - 상황에 맞게 값 변경하기
+        onDirectionRotate(rotate);
         Toast.makeText(getApplicationContext(), "경로 안내를 시작합니다.\n인터넷 연결이 끊어지지 않게 주의하세요.", Toast.LENGTH_LONG).show();
     }
 
     // function : 경로에 따라 유저이미지의 방향 회전시키기
     protected void onDirectionRotate(int value) {
         // left, right, front, back
-        direction.setRotation(41);
+        direction.setRotation(value);
 
     }
 }
